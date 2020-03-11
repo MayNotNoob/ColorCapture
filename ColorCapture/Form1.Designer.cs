14356b086b36e91a88fff9ac44caadd0b9ea9945
PaRaPuie <wangsichen725@gmail.com>
2020-03-11 19:21:01 +0100

    Add project files.

---
+++ ColorCapture/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ColorCapture
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.btnPicker = new System.Windows.Forms.Button();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(0, 0);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(111, 23);
+            this.btnCapture.TabIndex = 0;
+            this.btnCapture.Text = "Capture a color";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // btnPicker
+            // 
+            this.btnPicker.Location = new System.Drawing.Point(135, 0);
+            this.btnPicker.Name = "btnPicker";
+            this.btnPicker.Size = new System.Drawing.Size(111, 23);
+            this.btnPicker.TabIndex = 1;
+            this.btnPicker.Text = "Pick a color";
+            this.btnPicker.UseVisualStyleBackColor = true;
+            this.btnPicker.Click += new System.EventHandler(this.btnPicker_Click);
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(66, 29);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(103, 20);
+            this.txtColor.TabIndex = 3;
+            this.txtColor.Text = "#FFFFFF";
+            this.txtColor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(171, 27);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "Copy color";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Location = new System.Drawing.Point(0, 27);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(63, 23);
+            this.btnFormat.TabIndex = 5;
+            this.btnFormat.Text = "HEX :";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 52);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.btnPicker);
+            this.Controls.Add(this.btnCapture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Color Capture";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Button btnPicker;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnFormat;
+    }
+}
+
---
+++ ColorCapture/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ColorCapture
+{
+    public partial class MainForm : Form
+    {
+        private readonly List<Form> _forms = new List<Form>();
+        private Color _color;
+        private const string HEX = "HEX :";
+        private const string RGB = "RGB :";
+        public MainForm()
+        {
+            InitializeComponent();
+            InitBackgroundForm();
+        }
+
+        private void InitBackgroundForm()
+        {
+            foreach (Screen s in Screen.AllScreens)
+            {
+                Rectangle r = s.Bounds;
+                Form form = new Form
+                {
+                    Top = r.Top,
+                    Left = r.Left,
+                    Width = r.Width,
+                    Height = r.Height,
+                    TopMost = true,
+                    BackColor = Color.Azure,
+                    FormBorderStyle = FormBorderStyle.None,
+                    Opacity = 0.3d,
+                    StartPosition = FormStartPosition.Manual
+                };
+                form.MouseClick += (sender, args) =>
+                {
+                    _forms.ForEach(f => f.Hide());
+                    using (Bitmap bitmap = new Bitmap(form.Width, form.Height))
+                    {
+                        Point point = new Point(args.X, args.Y);
+                        using (Graphics g = Graphics.FromImage(bitmap))
+                        {
+                            g.CopyFromScreen(form.Left, form.Top, 0, 0, bitmap.Size);
+                        }
+                        _color = bitmap.GetPixel(args.X, args.Y);
+                        txtColor.Text = ColorToString(_color);
+                    }
+                };
+                _forms.Add(form);
+            }
+        }
+        private void btnPicker_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog.ShowDialog(this);
+            if (result == DialogResult.OK || result == DialogResult.Yes)
+            {
+                _color = colorDialog.Color;
+                txtColor.Text = ColorToString(_color);
+            }
+        }
+
+        private string ColorToString(Color c)
+        {
+            return btnFormat.Text == HEX ? $"#{c.R:X2}{c.G:X2}{c.B:X2}" : $"RGB({c.R},{c.G},{c.B})";
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(txtColor.Text);
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            _forms.ForEach(f => f.Show());
+        }
+
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            btnFormat.Text = btnFormat.Text == HEX ? RGB : HEX;
+            txtColor.Text = ColorToString(_color);
+        }
+    }
+}
